d7f5347294f7d394169b77deea0660f3b495fb7b
STrajkoska <62522354+STrajkoska@users.noreply.github.com>
2021-10-17 18:54:17 +0200

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    
+    menu.classList.remove('fa-times');
+    menu.classList.remove('active');
+
+}
